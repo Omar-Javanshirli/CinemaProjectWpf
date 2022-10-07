155ea1c39710afbe37fd2b6627ad690d572b59a6
Omer Cavanshirli <omrcvnsrli@hotmail.com>
2022-10-08 03:48:01 +0400

    new update

--- CinemaProjectWpf/View/BuyTicketWindow.xaml.cs
+++ CinemaProjectWpf/View/BuyTicketWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CinemaProjectWpf.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CinemaProjectWpf.UserContorls
+namespace CinemaProjectWpf.View
 {
     /// <summary>
-    /// Interaction logic for MovieCellUc.xaml
+    /// Interaction logic for BuyTicketWindow.xaml
     /// </summary>
-    public partial class MovieCellUc : UserControl
+    public partial class BuyTicketWindow : Window
     {
-        public MovieCellUc()
+        public BuyTicketWindow()
         {
             InitializeComponent();
-            this.DataContext = new MovieCellViewModel();
+
+            
         }
+
     }
 }
--- CinemaProjectWpf/ViewModel/MovieCellViewModel.cs
+++ CinemaProjectWpf/ViewModel/MovieCellViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CinemaProjectWpf.Model;
+﻿using CinemaProjectWpf.Command;
+using CinemaProjectWpf.Model;
+using CinemaProjectWpf.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,18 @@ namespace CinemaProjectWpf.ViewModel
     public class MovieCellViewModel:BaseViewModel
     {
         public Movie Movie { get; set; }
+        public RelayCommand BuyCommand { get; set; }
+
+        public MovieCellViewModel()
+        {
+            BuyCommand = new RelayCommand((e) =>
+            {
+                var view = new BuyTicketWindowViewModel();
+                var window =new  BuyTicketWindow();
+
+                window.Show();
+                
+            });
+        }
     }
 }
